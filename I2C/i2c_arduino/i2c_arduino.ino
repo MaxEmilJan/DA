74ce5f6795b7ae7a3fe6f6bf2b68cc44a69b26ff
max <max.emil.jan@mailbox.org>
2021-06-16 16:10:07 +0200

    different options for the lummination added

--- I2C/i2c_arduino/i2c_arduino.ino
+++ I2C/i2c_arduino/i2c_arduino.ino
@@ -41,20 +41,31 @@ void loop()
   if(ledState == 1) {
     digitalWrite(LED_BUILTIN, HIGH);
     for(int i=0; i<NUMPIXELS; i++) {
-      // set the color; warm white
-      ring.setPixelColor(i, ring.Color(255, 175, 45));
-      ring.show();
+      if((5 <= i && i < 11) || (17 <= i && i < 23)) {
+        // set the color; warm white
+        ring.setPixelColor(i, ring.Color(255, 255, 255));
+        ring.show(); 
+      }
+      else {
+        ring.setPixelColor(i, ring.Color(0, 0, 0));
+        ring.show(); 
+      }
     }
   }
   if(ledState == 2) {
     digitalWrite(LED_BUILTIN, HIGH);
     for(int i=0; i<NUMPIXELS; i++) {
-      // set the color; full brightness (cold)
-      ring.setPixelColor(i, ring.Color(255, 255, 255));
-      ring.show();
+      if((0 <= i && i < 5) || (11 <= i && i < 16)) {
+        // set the color; warm white
+        ring.setPixelColor(i, ring.Color(255, 255, 255));
+        ring.show(); 
+      }
+      else {
+        ring.setPixelColor(i, ring.Color(0, 0, 0));
+        ring.show(); 
+      }
     }
   }
-  else {}
 }
 
 void sendData(){
