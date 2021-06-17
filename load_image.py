0f895f4d3470ab1ee21a75ed68b9cd528583c53f
max <max.emil.jan@mailbox.org>
2021-06-17 16:53:12 +0200

    initial commit function to load image

---
+++ load_image.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+# function to load an image (will be replaced by the frame from the neoAPI later)
+def load_image(name_image):
+    # loading grayscaled image
+    img_raw = cv.imread(name_image)[...,0]
+    # select ROI and only keep this part
+    img_cut = img_raw[0:1200, 150:1790]
+    height, width = img_cut.shape
+    return img_cut, height, width
+
+# img, height, width = load_image("vignett_500µs.jpg")
+# cv.imwrite("test_roi_2.jpg", img)
