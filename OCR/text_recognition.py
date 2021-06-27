a729edd2493146643502be96ac2706670e8b697f
max <max.emil.jan@mailbox.org>
2021-06-27 08:50:05 +0200

    improved text recognition to recognize labels which are upside down

--- OCR/text_recognition.py
+++ OCR/text_recognition.py
@@ -17,10 +17,30 @@ def text_recognition(text_img, img_roi, square):
     if text_digit is not None:
         # only add the digits and not the "#"
         text_img = text_img + text_digit.group()[1:5]
+        print("fist attempt: match found")
     else:
-        pass
+        print("first attempt: no match found")
+        # if no match was detected, check if the label is upside down and try again
+        # get shape of ROI
+        height = img_roi_thresh.shape[0]
+        width = img_roi_thresh.shape[1]
+        # get center of ROI
+        center = (height/2, width/2)
+        # get rotation matrix
+        M = cv.getRotationMatrix2D(center, 180, 1.0)
+        # rotate ROI 180 degrees
+        img_rot = cv.warpAffine(img_roi_thresh, M, (height, width))
+        # repeat text recognition and pattern extraction
+        text_roi = pytesseract.image_to_string(img_rot)
+        text_digit = re.search(r"#(\d{4})", text_roi)
+        if text_digit is not None:
+            # only add the digits and not the "#"
+            text_img = text_img + text_digit.group()[1:5]
+            print("second attempt: match found")
+        else:
+            print("second attempt: no match found")
     #cv.imshow("ROI", img_roi)
     #cv.imshow("ROI thresh", img_roi_thresh)
     #cv.waitKey(0)
     #cv.destroyAllWindows()
-    return text_img #, img_box
+    return text_img
