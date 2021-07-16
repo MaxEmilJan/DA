c6758bca34c798a52cdd4d2ea629f89b230bbb3e
max <max.emil.jan@mailbox.org>
2021-07-16 10:14:20 +0200

    arduino script which constantly reads sensor output, turns on/off light and sends high/low to jetson to start the algorithm

--- Arduino/IR_Sensor_LED_Stripe/IR_Sensor_LED_Stripe.ino
+++ Arduino/IR_Sensor_LED_Stripe/IR_Sensor_LED_Stripe.ino
@@ -12,9 +12,13 @@ Adafruit_NeoPixel strip(NUMPIXELS, PIN_LED, NEO_GRB + NEO_KHZ800);
 
 // set the Address for the I2C connection to the Jetson (0x40 as default)
 int i2cAddress = 0x40;
-int ledState;
+// set IR-Sensor state
+int state_IR = 0;
 
 void setup() {
+  // join I2C Bus with address #0x40
+  Wire.begin(i2cAddress);
+  Wire.onRequest(requestEvent);
   // init serial output with given baud-rate
   Serial.begin (9600);
   // init Sensorpin
@@ -27,8 +31,8 @@ void loop() {
   // read signal from sensor
   bool val = digitalRead(PIN_IR);
   // if "low" (obstacle detected)
-  if (val == LOW){
-    Serial.println("low");
+  if (val == LOW) {
+    state_IR = 1;
     // turn the LED Stripe ON
     for(int i=0; i<NUMPIXELS-8; i++) {
       // set the bottom stripe to maximum intensity
@@ -39,17 +43,27 @@ void loop() {
       strip.setPixelColor(i, strip.Color(150, 150, 150));
     }
     strip.show();
-    //wait 5 seconds
-    delay(5000);
+    }
+    else {
+      state_IR = 0;
+      // turn the LED Stripe OFF
+      for(int i=0; i<NUMPIXELS; i++) {
+        // set the bottom stripe to maximum intensity
+        strip.setPixelColor(i, strip.Color(0, 0, 0));
+        strip.show();
+    }
+    }
+}
+
+// this function will be called when the master reads data from bus
+void requestEvent() {
+  // if "low" (obstacle detected)
+  if (state_IR == 1){
+    // send 1 to bus
+    Wire.write(1);
     }
   else {
-    Serial.println("high");
-    // turn the LED Stripe OFF
-    for(int i=0; i<NUMPIXELS; i++) {
-      // set the bottom stripe to maximum intensity
-      strip.setPixelColor(i, strip.Color(0, 0, 0));
-      strip.show();
+    // send 0 to bus
+    Wire.write(0);
     }
-    }
-  delay(500);
 }
