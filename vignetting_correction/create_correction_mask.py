5bfab481b8edbdb603a5b04f5b39ed6182aea787
max <max.emil.jan@mailbox.org>
2021-07-19 12:14:08 +0200

    renamed folder

---
+++ vignetting_correction/create_correction_mask.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+# loading plain image (white background) to measure vignetting
+img_vignett = cv.imread("vignett_1,5ms.jpg")[:,:,0]
+# select ROI and only keep this part
+img_roi = img_vignett[300:1075, 150:1790]
+# get the resolution of the image (1920x1200 in this case)
+height, width = img_roi.shape
+
+# normalize each pixel by deviding it by 255
+img_mask = np.zeros((height, width))
+
+for i in range(height):
+    for j in range(width):
+        if (img_roi[i,j] >= 60):
+            img_mask[i,j] = (img_roi[i,j]/255)
+        else:
+            img_mask[i,j] = 1
+
+# plot the correction mask
+plt.imshow(img_mask, cmap="gray")
+plt.colorbar()
+plt.show()
+
+# save the mask as an numpy array
+np.save('vignetting_correction_mask.npy', img_mask)
---
+++ vignetting_correction/vignetting_correction.py
@@ -0,0 +1,25 @@
+from numba import jit
+
+# use a just-in-time compilation to speed up the pixelwise for loops
+@jit(nopython=True)
+def vignetting_correction(img_vignett, correction_mask):
+    # get the size of the image (1920x1200 in this application)
+    height, width = img_vignett.shape
+    # vignetting-correction by deviding the vignetted image by the correction mask
+    img_corrected = img_vignett/correction_mask
+    # set every pixel with an intensity above 255 to 255
+    for i in range(height):
+        for j in range(width):
+            if (img_corrected[i,j] >= 255):
+                img_corrected[i,j] = int(255)
+            else:
+                img_corrected[i,j] = int(img_corrected[i,j])
+    # return the corrected image without vignetting
+    return img_corrected
+
+#vignett_mask = np.load("vignetting_correction_mask.npy")
+#img_roi = cv.imread("test_image.jpg")[...,0]
+#img_corrected = vignetting_correction(img_roi, vignett_mask)
+#cv.imwrite("test_corrected.jpg", img_corrected)
+#result = np.where(img_corrected == np.amax(img_corrected))
+#print(img_corrected[320, 1595])
