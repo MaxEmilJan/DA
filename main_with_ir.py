4cf14f76900e2523849a4806725f62b2ba7029c5
max <max.emil.jan@mailbox.org>
2021-07-22 13:30:09 +0200

    updated to run on the jetson

--- main_with_ir.py
+++ main_with_ir.py
@@ -1,18 +1,19 @@
 import smbus
 import sys
 import logging
+import time
 logging.basicConfig(level=logging.WARNING, format='File: %(filename)s, Line: %(lineno)d \nMessage: %(message)s')
 import numpy as np
 import cv2 as cv
 import neoapi
 import easyocr
 from load_frame import load_frame
-from ocr.vignetting_correction.vignetting_correction import vignetting_correction
+from vignetting_correction.vignetting_correction import vignetting_correction
 from ocr.preprocessing import preprocessing
 from ocr.edge_detection import canny_edge_detection
 from ocr.orientation_correction import orientation_correction
-#from ocr.binning import binning
-from ocr.text_recognition_gpu import text_recognition_gpu
+from ocr.binning import binning
+from ocr.text_recognition_easyocr import text_recognition_gpu
 
 
 # used camera parameters:
@@ -30,11 +31,12 @@ def main():
         address = 0x40
         # ---- OCR ----
         # load vignetting_correction_mask.npy to work with this array
-        vignett_mask = np.load("OCR/vignetting_correction/vignetting_correction_mask.npy")
+        vignett_mask = np.load("vignetting_correction/vignetting_correction_mask.npy")
         # create the necessary filters for morphologic operations
         filter_close = cv.getStructuringElement(cv.MORPH_RECT, (4,4))
         filter_dil = cv.getStructuringElement(cv.MORPH_RECT, (51,51))
         # define easyocr reader module
+        logging.warning("loading gpu module")
         reader = easyocr.Reader(['en'], gpu=True)
         # create a string to write the recognized text to
         text = ""
@@ -56,7 +58,7 @@ def main():
         camera.f.ExposureTime.Set(15000)
         # framerate set to max. 10 FPS
         camera.f.AcquisitionFrameRateEnable.value = True
-        camera.f.AcquisitionFrameRate.value = 2
+        camera.f.AcquisitionFrameRate.value = 10
         logging.warning("init camera done")
     except Exception as e:
         logging.error(e)
@@ -74,11 +76,12 @@ def main():
             text_prev = text
             # reset text so it does not append everything detected
             text = ""
+            # get the frame from the camera
             img_raw = camera.GetImage().GetNPArray()
             # cut the edges and grayscale
             img = load_frame(img_raw)
             # call the function to remove the vignetting
-            img_corrected = np.uint0(vignetting_correction(img, vignett_mask))
+            img_corrected = np.uint8(vignetting_correction(img, vignett_mask))
             # call the function to preprocess the image
             img_preprocessed = preprocessing(img_corrected)
             # call the function to detect edges
@@ -95,21 +98,19 @@ def main():
                         # deskew ROI
                         img_roi, box = orientation_correction(i, img_corrected)
                         # 2x2 binning the ROI to speed up the OCR
-                        #img_roi_bin = np.uint8(binning(img_roi, ((img_roi.shape[0]//2), (img_roi.shape[1]//2))))
-                        # text recognition in ROI
-                        text, match = text_recognition_gpu(text, img_roi, reader)
-                        # draw a box and the detected text to the original image, if a match was found
+                        img_roi_bin = np.uint8(binning(img_roi, ((img_roi.shape[0]//2), (img_roi.shape[1]//2))))
+                        # text recognition with easyocr
+                        text, match = text_recognition_gpu(text, img_roi_bin, reader)
+                        # if a match is found, evaluate its continuity
                         if match == True:
-                            box = np.int0(box)
                             # count the amount of equal texts recognized in a row
                             if text == text_prev:
                                 cnt += 1
-                            # dont reset the counter if no text was recognized, only when another fitting text got recognized
-                            elif text_prev == "":
-                                pass
+                                print(cnt)
                             # if another text which fits the pattern gets recognized, reset the counter
                             else:
                                 cnt = 0
+                                print("reset")
                         # if no match was found, do nothing
                         else:
                             pass
@@ -119,9 +120,9 @@ def main():
             # if no contours were detected, do nothing
             else:
                 pass
-            # check the contour
+            # check the counter
             # if the same four digits were recognized in a row, then make it the final result
-            if cnt == 4:
+            if cnt == 2:
                 logging.info("text recognition done")
                 logging.warning("result: "+str(text))
                 cnt = 0
